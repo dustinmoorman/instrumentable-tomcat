32363c4679125913922ed7f6e4f41d9265f32819
Dustin Moorman <dustin.moorman@appdynamics.com>
2020-04-23 10:20:05 -0500

    video

--- src/gui/debug.java
+++ src/gui/debug.java
@@ -33,7 +33,10 @@ public class debug extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		Date time = new Date();
 		out.println("<html>");
+    out.println("<script charset='UTF-8'> window['adrum-start-time'] = new Date().getTime(); (function(config){ config.appKey = 'EUM-AAB-AUM'; config.adrumExtUrlHttp = 'http://cdn.appdynamics.com';  config.adrumExtUrlHttps = 'https://cdn.appdynamics.com';  config.beaconUrlHttp = 'http://dmoormandevelopmentlab-kfk90btr.appd-sales.com:7001';    config.beaconUrlHttps = 'https://dmoormandevelopmentlab-kfk90btr.appd-sales.com:7002';    config.xd = {enable : true}; })(window['adrum-config'] || (window['adrum-config'] = {})); </script><script src='//cdn.appdynamics.com/adrum/adrum-4.5.17.2890.js'></script>");
 		out.println("<b>Debugging at " + time + "</b>");
+    out.println("<div>Embed tag </div><embed width='480' height='500' src='https://www.youtube.com/embed/il_t1WVLNxk'>");
+    out.println("<div>iframe</div><iframe width='560' height='315' src='https://www.youtube.com/embed/XD8kvGWhkPo' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
 		out.println("</html>");
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
